562f39fd59f0e24bd2a34585029610045f3f1c52
262038 <262038@1313-003>
2023-05-31 15:01:07 +0300

    Добавьте файлы проекта.

---
+++ TaxiAdminWpf/Components/PartialClass/Emplooy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TaxiAdminWpf.Components
+{
+    public partial class Emplooy
+    {
+        public string CollorBan
+        {
+            get
+            {
+                if(IsBan == 1)
+                {
+                    return "Red";
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+        public Visibility VisabilityBanAccept
+        {
+            get
+            {
+                if(IsBan == 1)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Hidden;
+                }
+            }
+        }
+        public Visibility VisabilityBanNo
+        {
+            get
+            {
+                if (IsBan == 1)
+                {
+                    return Visibility.Hidden;
+                }
+                else
+                {
+                    return Visibility.Visible;
+                }
+            }
+        }
+    }
+}
---
+++ TaxiClientWpf/Components/PartialClass/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiClientWpf.Components 
+{
+    public partial class Order
+    {
+      public  string Status
+        {
+            get
+            {
+                if (IsAccept == null)
+                {
+                    return "В обработке";
+                }
+                else if(IsAccept == 1)
+                {
+                    return "Подтвержден оператором";
+                }
+                else if (IsAccept == 2)
+                {
+                    return "Принят водителем";
+                }
+              
+                    return "в обработке";
+                
+            }
+        }
+        }
+        }
+    
+
